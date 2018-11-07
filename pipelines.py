73e79d206e5104cd9a5698107a79916729a99a63
dsc1697 <qqyvip009@163.com>
2018-11-07 17:57:17 +0800

    add lottery spider

---
+++ pipelines.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+
+
+class LotteryqlcPipeline(object):
+
+    def __init__(self):
+        # self.conn = pymysql.connect(host='103.27.5.156', user='developer', passwd='Developer!123', db='spider', charset='utf8')
+        self.conn = pymysql.connect(host='127.0.0.1', user='root', passwd='mysql', db='spider', charset='utf8')
+        self.cursor = self.conn.cursor()
+
+    def process_item(self, item, spider):
+        lottery_date = item['date']
+        issue = item['issue']
+        red1 = item['red1']
+        red2 = item['red2']
+        red3 = item['red3']
+        red4 = item['red4']
+        red5 = item['red5']
+        red6 = item['red6']
+        red7 = item['red7']
+        blue = item['blue']
+        sql = "insert into lottery_qlc(date, issue, red1, red2, red3, red4, red5, red6, red7, blue) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        self.cursor.execute(sql, (lottery_date, issue, red1, red2, red3, red4, red5, red6, red7, blue))
+        self.conn.commit()
+        return item
+
+    def close_spider(self, spider):
+        self.conn.close()
